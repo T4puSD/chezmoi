9e7647bd98ec8662cac93e3e3e4ec14212b722e8
Tom Payne <twpayne@gmail.com>
2020-05-21 17:13:28 +0100

    snapshot

---
+++ next/cmd/util_windows_test.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrimExecutableSuffix(t *testing.T) {
+	for _, filename := range []string{
+		"filename",
+		"filename.exe",
+		"filename.EXE",
+	} {
+		assert.Equal(t, "filename", trimExecutableSuffix(filename))
+	}
+}
